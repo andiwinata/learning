d633d0ca6b0da011a41dba78263454c3a4b3b7a2
andiwinata <andi.winata@hotmail.com>
2018-07-08 21:07:45 +1000

    Moving into 1 factory fn

--- Tasks/DatePicker/index.js
+++ Tasks/DatePicker/index.js
@@ -1,138 +1,169 @@
-
-const DEFAULT_DATE = new Date()
-const TOTAL_DAY_IN_WEEK = 7
-const DAY_NAME_MAPPING = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-
-// events onMonthChange (can also be onYearChange)
-// 
-
-const createDayNumber = ({ dayNum, disabled = false }) => {
-  const el = document.createElement('button')
-  el.classList.add('day', 'button', ...disabled ? ['disabled'] : [])
-  el.disabled = disabled
-  el.innerHTML = dayNum
-  return el
-}
-
-const createDayName = ({ dayName }) => {
-  const el = document.createElement('div')
-  el.classList.add('dayName')
-  el.innerHTML = dayName
-  return el
-}
-
-const createDates = ({ day,
-  dayNames,
-  firstDate,
-  lastDate,
-}) => {
-  const datesWrapper = document.createElement('div')
-
-  DAY_NAME_MAPPING.forEach(dayName => {
-    datesWrapper.appendChild(createDayName({ dayName }))
-  })
-
-  Array.from({ length: TOTAL_DAY_IN_WEEK * 5 }, (x, i) => i + 1).forEach(idDate => {
-    let disabled = false
-    if (idDate < firstDate || idDate > lastDate) {
-      disabled = true
-    }
-    const dateEl = createDayNumber({
-      dayNum: !disabled ? idDate - firstDate + 1: '',
-      disabled,
-    })
-    datesWrapper.appendChild(dateEl)
-  })
-
-  return datesWrapper
-}
-
-const createYearMonth = ({ month, year, monthNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-] }) => {
-  const yearWrap = document.createElement('div')
-  yearWrap.classList.add('yearWrap')
-
-  const prevMonthBtn = document.createElement('button')
-  prevMonthBtn.innerText = 'Prev'
-  prevMonthBtn.classList.add('navigateMonth', 'button')
-
-  const nextMonthBtn = document.createElement('button')
-  nextMonthBtn.innerText = 'Next'
-  nextMonthBtn.classList.add('navigateMonth', 'button')
-
-  const yearText = document.createElement('span')
-  yearText.classList.add('yearText')
-  yearText.innerText = `${monthNames[month]} - ${year}`
-
-  yearWrap.appendChild(prevMonthBtn)
-  yearWrap.appendChild(yearText)
-  yearWrap.appendChild(nextMonthBtn)
-
-  return yearWrap
-}
-
 const createCalendar = ({
-  year,
-  month,
-  day,
-  firstDate,
-  lastDate,
+  initialDate = new Date(),
+  dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  querySelector,
 }) => {
-  // create calendar container if doesn't exist
-  // auto add it to the body
-  let calendarContainer = document.getElementById('calendar')
-  if (!calendarContainer) {
-    calendarContainer = document.createElement('div')
-    calendarContainer.setAttribute('id', 'calendar')
-    document.body.appendChild(calendarContainer)
+  const TOTAL_DAY_IN_WEEK = 7
+
+  // events onMonthChange (can also be onYearChange)
+  // TODO { el, update } refactor
+
+  const createDayNumberRender = (opts) => {
+    const el = document.createElement('button')
+
+    const update = ({ dayNum, disabled = false }) => {
+      el.classList.add('day', 'button', ...disabled ? ['disabled'] : [])
+      el.disabled = disabled
+      el.textContent = dayNum
+    }
+
+    update(opts) // init
+
+    return {
+      el,
+      update,
+    }
   }
-  calendarContainer.classList.add('calendar')
 
-  // create year and month elements with its wrapper
-  calendarContainer.appendChild(createYearMonth({
+  const createDayNameRender = (opts) => {
+    const el = document.createElement('div')
+    const update = ({ dayName }) => {
+      el.classList.add('dayName')
+      el.textContent = dayName
+    }
+
+    update(opts) // init
+
+    return {
+      el,
+      update
+    }
+  }
+
+  const createDatesRender = ({ daysArray }) => {
+    const datesWrapper = document.createElement('div')
+
+    // create day names
+    dayNames.forEach(dayName => {
+      datesWrapper.appendChild(createDayNameRender({ dayName }).el)
+    })
+
+    // create day numbers
+    daysArray.forEach(day => {
+      const dateEl = createDayNumberRender({
+        dayNum: day,
+        disabled: day === '',
+      })
+      datesWrapper.appendChild(dateEl.el)
+    })
+
+    return datesWrapper
+  }
+
+  const createYearMonthRender = ({ month, year,
+    prevMonthOnClick, nextMonthOnClick
+  }) => {
+    const yearWrap = document.createElement('div')
+    yearWrap.classList.add('yearWrap')
+
+    const prevMonthBtn = document.createElement('button')
+    prevMonthBtn.innerText = 'Prev'
+    prevMonthBtn.classList.add('navigateMonth', 'button')
+    prevMonthBtn.addEventListener('click', prevMonthOnClick)
+
+    const nextMonthBtn = document.createElement('button')
+    nextMonthBtn.innerText = 'Next'
+    nextMonthBtn.classList.add('navigateMonth', 'button')
+    nextMonthBtn.addEventListener('click', nextMonthOnClick)
+
+    const yearText = document.createElement('span')
+    yearText.classList.add('yearText')
+    yearText.innerText = `${monthNames[month]} - ${year}`
+
+    yearWrap.appendChild(prevMonthBtn)
+    yearWrap.appendChild(yearText)
+    yearWrap.appendChild(nextMonthBtn)
+
+    return yearWrap
+  }
+
+  const createCalendarRender = ({
     year,
     month,
-  }))
-
-  // create dates and its wrapper
-  calendarContainer.appendChild(createDates({
     day,
-    firstDate,
-    lastDate,
-  }))
+    daysArray,
+  }) => {
+    // create calendar container if doesn't exist
+    // auto add it to the body
+    let calendarContainer = document.querySelector(querySelector)
+    calendarContainer.classList.add('calendar')
 
-  return calendarContainer
-}
+    // create year and month elements with its wrapper
+    calendarContainer.appendChild(createYearMonthRender({
+      year,
+      month,
+    }))
 
-const init = () => {
-  const year = DEFAULT_DATE.getFullYear()
-  const month = DEFAULT_DATE.getMonth()
-  const day = DEFAULT_DATE.getDay()
+    // create dates and its wrapper
+    calendarContainer.appendChild(createDatesRender({
+      day,
+      daysArray
+    }))
 
-  // get first and last day of month
-  const firstDate = new Date(year, month, 1).getDate()
-  const lastDate = new Date(year, month + 1, 0).getDate()
+    return calendarContainer
+  }
 
-  createCalendar({
-    year,
-    month,
-    day,
+  /**
+   * Generates array of days in for the calendar
+   * will contain empty for some elements
+   */
+  const generateDaysArray = ({
     firstDate,
     lastDate
+  }) => Array.from({ length: TOTAL_DAY_IN_WEEK * 5 }, (x, i) => i + 1).map(dayId =>
+    // return empty if day index is less than first date
+    dayId < firstDate || dayId > lastDate ? '' : dayId - firstDate + 1
+  )
+
+  const getFirstLastDate = ({ year, month }) => ({
+    firstDate: new Date(year, month, 1).getDate(),
+    lastDate: new Date(year, month + 1, 0).getDate(),
   })
+
+  const init = () => {
+    const year = initialDate.getFullYear()
+    const month = initialDate.getMonth()
+    const day = initialDate.getDay()
+
+    // get first and last day of month
+    const { firstDate, lastDate } = getFirstLastDate({ year, month })
+    const daysArray = generateDaysArray({ firstDate, lastDate })
+
+    createCalendarRender({
+      year,
+      month,
+      day,
+      daysArray,
+    })
+  }
+
+  init()
 }
 
-init()
-
+createCalendar({
+  querySelector: '#calendar'
+})
